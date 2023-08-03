12c9571166bef250cb70cc03c883271f74f6d4c6
skwizo <skw9izo6@gmail.com>
2023-08-03 11:42:55 +0100

    v4

--- js/edit employee app.js
+++ js/edit employee app.js
@@ -9,3 +9,35 @@ fileInput.addEventListener('change', (event) => {
         fileLabel.textContent = 'upload a picture of the employee. PNG or JPG';
     }
 });
+
+
+let inputs = document.querySelectorAll('.select-container');
+
+inputs.forEach(input => {
+    let selected = input.querySelector('.selected-item');
+    let options = input.querySelectorAll('.option');
+    let menu = input.querySelector('.select');
+
+    selected.addEventListener('click', () => {
+        inputs.forEach(otherInput => {
+            if (otherInput !== input) {
+                otherInput.querySelector('.select').classList.remove('show-menu');
+            }
+        });
+        menu.classList.toggle('show-menu');
+    });
+
+    menu.addEventListener('mouseleave', () => {
+        menu.classList.remove('show-menu');
+    });
+
+    options.forEach(option => {
+        option.addEventListener('click', () => {
+            options.forEach(option => {
+                option.classList.remove('selected');
+            });
+            selected.querySelector('p').textContent = option.querySelector('.opt-radio').value;
+            option.classList.add('selected');
+        });
+    });
+});
