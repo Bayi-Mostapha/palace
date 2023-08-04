6c22443e1fd6ae4f0870fe4d64157836f6d66bb8
skwizo <skw9izo6@gmail.com>
2023-08-04 15:03:12 +0100

    v5 -- i will seperate deferent elements with their styles into seperate folders

--- general/js/dashboard app.js
+++ general/js/dashboard app.js
@@ -2,8 +2,7 @@ function updateTime() {
     const currentTime = new Date();
     const hours = currentTime.getHours();
     const minutes = currentTime.getMinutes();
-    const formattedTime = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" /></svg> 
-                    ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
+    const formattedTime = `<i class="fa-regular fa-clock"></i> ${hours}:${minutes < 10 ? '0' : ''}${minutes}`;
 
     document.querySelector('.current-time').innerHTML = formattedTime;
 }
@@ -26,8 +25,7 @@ function displayDate() {
     const month = getMonthName(currentDate.getMonth());
     const year = currentDate.getFullYear();
 
-    const formattedDate = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M152 24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H64C28.7 64 0 92.7 0 128v16 48V448c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192 144 128c0-35.3-28.7-64-64-64H344V24c0-13.3-10.7-24-24-24s-24 10.7-24 24V64H152V24zM48 192H400V448c0 8.8-7.2 16-16 16H64c-8.8 0-16-7.2-16-16V192z"/></svg>
-                        ${day} ${month}, ${year}`;
+    const formattedDate = `<i class="fa-regular fa-calendar"></i> ${day} ${month}, ${year}`;
 
     document.querySelector('.dateDisplay').innerHTML = formattedDate;
 }
@@ -104,14 +102,14 @@ function grContent(chartData) {
     return `
         <p>
             Last month:
-            <svg style="fill:${lastMonth > 0 ? 'var(--succes)' : 'var(--canceled)'};transform: rotate(${lastMonth > 0 ? '-90deg' : '90deg'});" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            <i style="color:${lastMonth > 0 ? 'var(--succes)' : 'var(--canceled)'};transform: rotate(${lastMonth > 0 ? '-90deg' : '90deg'});" class="fa-solid fa-arrow-right"></i>
             <span style="color:${lastMonth > 0 ? 'var(--succes)' : 'var(--canceled)'}">
                 ${lastMonth > 0 ? '+' : ''}${lastMonth}%
             </span>
             </p>
         <p>
             Last 6 months:
-            <svg style="fill:${last6Months > 0 ? 'var(--succes)' : 'var(--canceled)'};transform: rotate(${last6Months > 0 ? '-90deg' : '90deg'});" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/></svg>
+            <i style="color:${last6Months > 0 ? 'var(--succes)' : 'var(--canceled)'};transform: rotate(${last6Months > 0 ? '-90deg' : '90deg'});" class="fa-solid fa-arrow-right"></i>
             <span style="color:${last6Months > 0 ? 'var(--succes)' : 'var(--canceled)'}">
                 ${last6Months > 0 ? '+' : ''}${last6Months}%
             </span>
