0f9d55ad87293191278a5bc423f260288450bda5
skwizo <bayimostapha@gmail.com>
2023-08-16 16:54:07 +0100

    finished login

--- js/login-sign-up.js
+++ js/login-sign-up.js
@@ -12,5 +12,55 @@ passwords.forEach(password => {
             pwdInput.type = 'password';
             pwdBtn.innerHTML = '<i class="fa-regular fa-eye"></i>';
         }
-    })
-})
+    });
+});
+
+// const carouselBtns = document.querySelectorAll('.manual-btn');
+// const radio1 = document.querySelector('#radio1');
+// const radio2 = document.querySelector('#radio2');
+// const radio3 = document.querySelector('#radio3');
+// if (radio1.checked)
+//     document.querySelector('.radio1-l').style.backgroundColor = 'white';
+// radio1.addEventListener('change', () => {
+//     rmBg();
+//     document.querySelector('.radio1-l').style.backgroundColor = 'white';
+// });
+// radio2.addEventListener('change', () => {
+//     rmBg();
+//     document.querySelector('.radio2-l').style.backgroundColor = 'white';
+
+// });
+// radio3.addEventListener('change', () => {
+//     rmBg();
+//     document.querySelector('.radio3-l').style.backgroundColor = 'white';
+// });
+// function rmBg() {
+//     carouselBtns.forEach(carouselBtn => {
+//         carouselBtn.style.backgroundColor = 'transparent';
+//     });
+// }
+
+const inputs = document.querySelectorAll('.input');
+inputs.forEach(input => {
+    const inputTXT = input.querySelector('input');
+    const label = input.querySelector('p');
+
+    inputTXT.addEventListener('input', (e) => {
+        if (e.value !== '') {
+            label.classList.add('label');
+            label.classList.remove('placeholder');
+        } else {
+            label.classList.remove('label');
+            label.classList.add('placeholder');
+        }
+    });
+    inputTXT.addEventListener('blur', () => {
+        if (inputTXT.value !== '') {
+            label.classList.add('label');
+            label.classList.remove('placeholder');
+        } else {
+            label.classList.remove('label');
+            label.classList.add('placeholder');
+        }
+    });
+});
